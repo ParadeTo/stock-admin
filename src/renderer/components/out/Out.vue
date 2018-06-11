30130e62739bb6c75e689bc36a29b786b847c92f
youxingzhi <xingzhi.you@shopee.com>
2018-06-11 15:30:09 +0800

    初步完成出库

--- src/renderer/components/out/Out.vue
+++ src/renderer/components/out/Out.vue
@@ -1,5 +1,81 @@
 <template>
-  <div>
-    out
-  </div>
+  <el-table
+    :data="goods"
+    border
+    style="width: 100%">
+    <el-table-column
+      prop="code"
+      label="商品编号">
+    </el-table-column>
+    <el-table-column
+      prop="name"
+      label="商品名称">
+    </el-table-column>
+    <el-table-column
+      prop="type"
+      label="规格型号">
+    </el-table-column>
+    <el-table-column
+      prop="number"
+      label="数量">
+    </el-table-column>
+    <el-table-column
+      label="操作">
+      <template slot-scope="scope">
+        <el-button @click="handleOutClick(scope.row)" type="text" size="small">出库</el-button>
+      </template>
+    </el-table-column>
+  </el-table>
 </template>
+
+<script>
+  export default {
+    data () {
+      return {
+        goods: []
+      }
+    },
+
+    async created () {
+      this.getGoods()
+    },
+
+    methods: {
+      async getGoods () {
+        const goods = await this.$db.goods.getAllGoods()
+        this.goods = goods
+      },
+
+      handleOutClick (row) {
+        const { name, _id, number } = row
+
+        this.$prompt(`${name}，请输入出库数量`, '出库', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          inputPattern: /^[123456789]+/,
+          inputErrorMessage: '出库数量不正确'
+        }).then(async ({ value }) => {
+          value = value > number ? number : value
+
+          if (number === Number(value)) {
+            console.log(1)
+            await this.$db.goods.removeItem(_id)
+          } else {
+            await this.$db.goods.stockOut(_id, value)
+          }
+
+          this.$message({
+            type: 'success',
+            message: `${name} 出库成功 ${value} 件`,
+            onClose: () => { this.getGoods() }
+          })
+        }).catch(() => {
+          this.$message({
+            type: 'info',
+            message: '取消出库'
+          })
+        })
+      }
+    }
+  }
+</script>
--- src/renderer/datastore/index.js
+++ src/renderer/datastore/index.js
@@ -10,6 +10,7 @@ db.goods = new Datastore({
   filename: path.join(remote.app.getPath('userData'), '/goods.db')
 })
 
+// goods
 db.goods.getAllGoodsNameAndType = () => {
   const names = new Set()
   const types = new Set()
@@ -28,6 +29,43 @@ db.goods.getAllGoodsNameAndType = () => {
       } else {
         reject(err)
       }
+    })
+  })
+}
+
+db.goods.getAllGoods = () => {
+  return new Promise((resolve, reject) => {
+    db.goods.find({}, (err, goods) => {
+      if (err) {
+        reject(err)
+        return
+      }
+      resolve(goods)
+    })
+  })
+}
+
+db.goods.stockOut = (_id, number) => {
+  return new Promise((resolve, reject) => {
+    db.goods.update({ _id }, { $inc: { number: -number } }, {}, (err, goods) => {
+      if (err) {
+        reject(err)
+        return
+      }
+      resolve(goods)
+    })
+  })
+}
+
+db.goods.removeItem = _id => {
+  return new Promise((resolve, reject) => {
+    db.goods.remove({ _id }, {}, (err, goods) => {
+      if (err) {
+        console.log(err)
+        reject(err)
+        return
+      }
+      resolve(goods)
     })
   })
 }
